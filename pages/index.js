1d2f2b000bd9e8dedede0a8373eaed68dbe6c6af
Nazar <nazar.poplavsky@gmail.com>
2023-05-08 15:36:35 +0200

    Basic logig was implemented

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,62 @@
 import { useEffect, useState } from "react";
+import { Montserrat } from "next/font/google";
+
+const font = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
-	const [options, setOptions] = useState([]);
-	const [currentOption, setCurrentOption] = useState({});
-	const [order, setOrder] = useState([]);
+	const [components, setComponent] = useState([]);
+	const [choosenOptions, setChoosenOptions] = useState([]);
 	const [currentStep, setCurrentStep] = useState(0);
 
 	useEffect(() => {
 		fetch("/db/pizzaOptions.json")
 			.then((res) => res.json())
-			.then((data) => setOptions(data));
+			.then((data) => setComponent(data));
 	}, []);
 	useEffect(() => {
-		setCurrentOption(
-			options?.filter((_option) => {
-				if (_option.id === currentStep) {
-					return _option;
-				}
-			})[0]
-		);
-	}, [options, currentStep]);
-	useEffect(() => {
-		const defaultOrder = [];
-		options.forEach((_option) => defaultOrder.push({ id: _option.id, name: _option.name, choosenOpt: _option.options[0] }));
+		const newOptions = components.map((_component) => ({
+			componentId: _component.id,
+			componentName: _component.name,
+			choosenOpt: _component.options[0],
+		}));
+		setChoosenOptions(newOptions);
+	}, [components]);
 
-		setOrder(defaultOrder);
-	}, [options]);
+	const currentComponent = components.find((_component) => _component.id === currentStep) || {};
+	const currentComponentChoosenOptions =
+		choosenOptions.find((choosenOption) => choosenOption.componentId === currentComponent.id) || {};
 
 	return (
-		<main className="grid grid-rows-[auto,auto,1fr,auto,64px] overflow-hidden bg-amber-900 pt-4 md:grid-cols-[minmax(0px,256px),1fr,minmax(0px,256px)] md:grid-rows-[auto,1fr,auto] md:gap-8 md:px-6 md:py-6 lg:px-20 lg:py-20">
-			<Steps options={options} currentStep={currentStep} />
-			<Options currentOption={currentOption} order={order} setOrder={setOrder} />
+		<main
+			className={`grid grid-rows-[auto,auto,1fr,auto,64px] gap-8 overflow-hidden bg-amber-900 pt-4 md:grid-cols-[minmax(160px,16%),1fr,minmax(160px,16%)] md:grid-rows-[auto,1fr,auto] md:px-6 md:py-6 lg:px-20 lg:py-20 ${font.className}`}
+		>
+			<Steps components={components} currentStep={currentStep} setCurrentStep={setCurrentStep} />
+			<Options
+				currentComponent={currentComponent}
+				choosenOptions={choosenOptions}
+				setChoosenOptions={setChoosenOptions}
+				currentComponentChoosenOptions={currentComponentChoosenOptions}
+			/>
 			<Model />
-			<Navigation options={options} currentStep={currentStep} setCurrentStep={setCurrentStep} />
-			<Recipe order={order} />
+			<Navigation components={components} currentStep={currentStep} setCurrentStep={setCurrentStep} />
+			<Recipe choosenOptions={choosenOptions} />
 		</main>
 	);
 }
 
-export function Steps({ options, currentStep }) {
+export function Steps({ components, currentStep, setCurrentStep }) {
+	const chooseStep = (stepId) => {
+		setCurrentStep(stepId);
+	};
+
 	return (
-		<section className="flex overflow-x-scroll md:col-start-1 md:row-start-1 md:row-end-4 md:self-center md:justify-self-start">
+		<section className="flex overflow-x-hidden md:col-start-1 md:row-start-1 md:row-end-4 md:self-center md:justify-self-start">
 			<ul className="flex flex-row gap-6 md:flex-col">
-				{options?.map((_option) => (
-					<li className={_option.id === currentStep ? "text-white" : "text-amber-600"} key={_option.id}>
-						{_option.name}
+				{components.map((_component) => (
+					<li className={_component.id === currentStep ? "text-white" : "text-amber-600"} key={_component.id}>
+						<a href="#" onClick={() => chooseStep(_component.id)}>
+							<span className="uppercase">{_component.name}</span>
+						</a>
 					</li>
 				))}
 			</ul>
@@ -52,19 +64,33 @@ export function Steps({ options, currentStep }) {
 	);
 }
 
-export function Options({ currentOption, order, setOrder }) {
+export function Options({ currentComponent, choosenOptions, setChoosenOptions, currentComponentChoosenOptions }) {
+	const updateChoosenOptions = (currentComponentOption) => {
+		const updatedOptions = choosenOptions.map((_choosenOption) =>
+			_choosenOption.componentId === currentComponent.id
+				? { ..._choosenOption, choosenOpt: currentComponentOption }
+				: _choosenOption
+		);
+		setChoosenOptions(updatedOptions);
+	};
+
 	return (
-		<section className="md:col-start-1 md:col-end-4 md:row-start-1 ">
-			<div className="text-center text-amber-600">Choose {currentOption?.name}</div>
+		<section className="md:col-start-1 md:col-end-4 md:row-start-1">
+			<div className="mb-2 text-center text-amber-600">
+				Choose <span className="capitalize">{currentComponent.name}</span>
+			</div>
 			<ul className="flex justify-center gap-6 text-white">
-				{currentOption?.options?.map((_option) => (
-					<li
-						onClick={() =>
-							setOrder(order?.map((_item) => (_item.id === currentOption.id ? { ..._item, choosenOpt: _option } : _item)))
-						}
-						key={_option.id}
-					>
-						{_option.name}
+				{currentComponent.options?.map((_currentComponentOption) => (
+					<li key={_currentComponentOption.id}>
+						<a href="#" onClick={() => updateChoosenOptions(_currentComponentOption)}>
+							<span
+								className={`text-xl uppercase ${
+									currentComponentChoosenOptions.choosenOpt?.id === _currentComponentOption.id ? "text-white" : "text-amber-600"
+								}`}
+							>
+								{_currentComponentOption.name}
+							</span>
+						</a>
 					</li>
 				))}
 			</ul>
@@ -76,51 +102,73 @@ export function Model() {
 	return <section className="md:col-start-2 md:row-start-2">Model</section>;
 }
 
-export function Navigation({ options, currentStep, setCurrentStep }) {
+export function Navigation({ components, currentStep, setCurrentStep }) {
 	const prevStep = () => currentStep > 0 && setCurrentStep(currentStep - 1);
-	const nextStep = () => currentStep < options?.length - 1 && setCurrentStep(currentStep + 1);
+	const nextStep = () => currentStep < components.length - 1 && setCurrentStep(currentStep + 1);
 
 	return (
 		<section className="flex w-full justify-center gap-4 justify-self-center md:col-start-2 md:col-end-2 md:row-start-3 md:row-end-3 md:self-end">
-			<button onClick={prevStep} className="flex h-12 w-12 items-center justify-center rounded-3xl bg-white">
-				Prev
+			<button onClick={prevStep} className="flex h-12 w-12 items-center justify-center rounded-3xl bg-white shadow-xl">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.5}
+					stroke="currentColor"
+					className="h-6 w-6"
+				>
+					<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+				</svg>
 			</button>
 			<button
 				onClick={nextStep}
-				className="flex h-12 max-w-[256px] flex-1 items-center justify-center rounded-3xl bg-white px-8"
+				className="relative flex h-12 max-w-[256px] flex-1 items-center justify-center rounded-3xl bg-white px-8 shadow-xl"
 			>
 				Next
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.5}
+					stroke="currentColor"
+					className="absolute  right-3 h-6 w-6"
+				>
+					<path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+				</svg>
 			</button>
 		</section>
 	);
 }
 
-export function Recipe({ order }) {
-	const [price, setPrice] = useState();
+export function Recipe({ choosenOptions }) {
+	const [total, setTotal] = useState();
 
 	useEffect(() => {
-		const pricesArray = [];
-		order.forEach((_item) => pricesArray.push(_item.choosenOpt.price));
-		setPrice(pricesArray.reduce((a, b) => a + b, 0));
-	}, [order]);
+		const pricesArray = choosenOptions.map((choosenOption) => choosenOption.choosenOpt?.price || 0);
+		const totalPrice = pricesArray.reduce((a, b) => a + b, 0);
+		setTotal(totalPrice);
+	}, [choosenOptions]);
 
 	return (
-		<section className="absolute inset-x-0 bottom-0 flex translate-y-[calc(100%-64px)] flex-col items-center rounded-t-xl  bg-stone-300 px-5 pb-8 pt-3 hover:translate-y-0 md:static md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-4 md:translate-y-0 md:self-end md:bg-transparent md:p-0">
+		<section className="absolute inset-x-0 bottom-0 flex translate-y-[calc(100%-64px)] flex-col items-center rounded-t-xl bg-stone-300 px-5 pb-8 pt-3 hover:translate-y-0 md:static md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-4 md:translate-y-0 md:self-end md:bg-transparent md:p-0">
 			<span className="mb-4 h-1 w-20 rounded bg-white md:hidden"></span>
-			<div className="flex w-full flex-col gap-4">
-				<div>Twoje zamówienie</div>
+			<div className="flex w-full flex-col gap-4 text-amber-950 md:text-white">
+				<div className="">Your order</div>
 				<hr className="" />
 				<ul className="flex flex-col gap-4">
-					{order.map((_item) => (
-						<li>
-							{_item.name}:{_item.choosenOpt.name}
+					{choosenOptions.map((_choosenOption) => (
+						<li key={_choosenOption.componentId}>
+							<span className="capitalize">
+								<span className="text-amber-600">{_choosenOption.componentName}:</span>{" "}
+								<span className="">{_choosenOption.choosenOpt?.name}</span>
+							</span>
 						</li>
 					))}
 				</ul>
 				<hr />
 				<div className="flex justify-between">
-					<div>Razem:</div>
-					<div>{price}</div>
+					<span>Total</span>
+					<span>{total}</span>
 				</div>
 			</div>
 		</section>
