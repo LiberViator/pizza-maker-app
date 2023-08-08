719a08530a1ebd4bd539c8be74e8306fa09c3935
Nazar <nazar.poplavsky@gmail.com>
2023-08-08 10:25:37 +0200

    GitHub Pages deploy fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
 	reactStrictMode: true,
 	basePath: "/pizza-maker-app",
+	images: {
+		loader: "akamai",
+		path: "",
+	},
+	assetPrefix: "./",
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef, useLayoutEffect } from "react";
+import Link from "next/link";
 import { Montserrat } from "next/font/google";
 
 import PizzaCanvas from "@/components/PizzaCanvas";
